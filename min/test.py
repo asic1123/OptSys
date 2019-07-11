b374c2ea023129b7145c4a8932735eb02c1f3cdc
asic1123 <asic1123@163.com>
2019-07-10 22:05:34 -0400

    add setting file named scanrule

--- min/test.py
+++ min/test.py
@@ -1,6 +1,23 @@
 import raytracing as rt
 import visualize as vis
 import numpy as np
+import re
+
+pattern = re.compile('scanangle')
+
+with open('scanrule','r') as f:
+        rulename=f.readlines()
+        rule=[]
+        for line in rulename:
+            m=pattern.match(line)
+            if m:
+                rule.append(line.strip('\n'))
+
+angle=[]
+for i in rule:
+    angle.append(int(i.split(':')[1]))
+
+f.closed
 
 components = []
 components.append(rt.Mirror(
@@ -13,14 +30,16 @@ components.append(rt.Mirror(
 			  pos=[300,0],
 			  theta=0))
 rays = []
-rays.append([125, 100, -np.pi/3])
-rays.append([75, 100, -np.pi*2/3])
+n1 = 180/float(angle[0])
+n2 = 180/float(angle[1])
+rays.append([75, 100, -(np.pi/2 - np.pi/n1)])
+rays.append([125, 100, -(np.pi/2 - np.pi/n2)])
 ray_bundles = rt.propagate_rays(components, rays)
 
 
 
 # Color for the rays
-colors = ['r','r']
+colors = ['r','b']
 
 # Create a new canvas
 canvas = vis.Canvas([-100, 300], [-100, 100])
